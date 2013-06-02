dfe0d0935ec13703b1fe0ed02cf4cdbe1ef93b51
Charles <charles@reptile.ca>
2013-06-02 10:23:06 -0700

    A native Go clean room implementation of the Porter Stemming Algorithm.

---
+++ README.md
@@ -0,0 +1,28 @@
+# Go Porter Stemmer
+
+A native Go clean room implementation of the Porter Stemming Algorithm.
+
+This is NOT a port. This is a native Go implementation from the human-readable
+description of the algorithm.
+
+I've tried to make it (more) efficient by NOT internally using string's, but
+instead internally using []rune's and using the same (array) buffer used by
+the []rune slice (and sub-slices) at all steps of the algorithm.
+
+For Porter Stemmer algorithm, see:
+http://tartarus.org/martin/PorterStemmer/def.txt
+http://tartarus.org/martin/PorterStemmer/
+
+Also, since when I initially implemented it, it failed the tests at...
+
+http://tartarus.org/martin/PorterStemmer/voc.txt
+http://tartarus.org/martin/PorterStemmer/output.txt
+
+... I then looked at the source code here...
+
+http://tartarus.org/martin/PorterStemmer/c.txt
+
+... and noticed that there are some items marked as "DEPARTURE",
+which differ from the original algorithm.
+
+Implemented these departures too.
---
+++ porterstemmer_stem_test.go
@@ -0,0 +1,71 @@
+package porterstemmer
+
+
+
+import (
+	"bufio"
+	"os"
+	"strings"
+    "testing"
+)
+
+
+
+func TestStem(t *testing.T) {
+
+	vocFileName := "testdata/voc.txt"
+	vocFd, err := os.Open(vocFileName)
+	if nil != err {
+		t.Errorf("Could NOT open testdata file: [%s]. Received error: [%v]", vocFileName, err)
+/////// RETURN
+		return
+	}
+	defer vocFd.Close()
+
+	voc := bufio.NewReaderSize(vocFd, 1024)
+
+
+
+	outFileName := "testdata/output.txt"
+	outFd, err := os.Open(outFileName)
+	if nil != err {
+		t.Errorf("Could NOT open testdata file: [%s]. Received error: [%v]", outFileName, err)
+/////// RETURN
+		return
+	}
+	defer outFd.Close()
+
+	out := bufio.NewReaderSize(outFd, 1024)
+
+
+
+	for {
+
+		vocS, err := voc.ReadString('\n')
+		if nil != err {
+	/////// BREAK
+			break
+		}
+
+		vocS = strings.Trim(vocS, "\n\r\t ")
+
+
+
+		expected, err := out.ReadString('\n')
+		if nil != err {
+			t.Errorf("Received unexpected error when trying to read a line from [%s]. Received error: [%v]", outFileName, err)
+	/////// BREAK
+			break
+
+		}
+
+		expected = strings.Trim(expected, "\n\r\t ")
+
+
+
+		actual := StemString(vocS)
+		if expected != actual {
+			t.Errorf("Input: [%s] -> Actual: [%s]. Expected: [%s]", vocS, actual, expected)
+		}
+	}
+}
