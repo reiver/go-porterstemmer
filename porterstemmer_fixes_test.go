db4882678e90bcb2a09a5f5d9cd8721eb56f698c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-04-11 23:14:03 -0700

    added test

--- porterstemmer_fixes_test.go
+++ porterstemmer_fixes_test.go
@@ -22,3 +22,19 @@ func TestStemStringIon(t *testing.T) {
 		t.Errorf("Input: [%s] -> Actual: [%s]. Expected: [%s]", s, actual, expected)
 	}
 }
+
+
+// Test for issue listed here:
+// https://github.com/reiver/go-porterstemmer/pull/10
+//
+// StemString("eeg") was causing runtime exception
+func TestStemStringEeg(t *testing.T) {
+
+	expected := "eeg"
+
+	s := "eeg"
+	actual := StemString(s)
+	if expected != actual {
+		t.Errorf("Input: [%s] -> Actual: [%s]. Expected: [%s]", s, actual, expected)
+	}
+}
